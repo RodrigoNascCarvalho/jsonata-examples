f724c7d83c16bb955926c1d38bc36f8c0ccbb766
Rodrigo Nascimento de Carvalho <rnascimentodecarva@unomaha.edu>
2019-04-02 11:25:37 -0300

    Adding colors to the output for specific SPEC file.

--- __tests__/jsonata-helper.js
+++ __tests__/jsonata-helper.js
@@ -1,14 +1,15 @@
 const jsonata = require('jsonata')
+const colors = require('colors/safe')
 
 const log = (...args) => (process.env.DEBUG ? console.log(...args) : () => {})
 
 module.exports = {
     testJsonataExpr: (inputJson, exprStr) => {
-        log('Input:', JSON.stringify(inputJson, null, 2))
-        log('Expr:', exprStr)
+        log(colors.yellow('Input:'), colors.yellow.italic(JSON.stringify(inputJson, null, 2)))
+        log(colors.cyan('Expr:'), colors.cyan.underline(exprStr))
         const expression = jsonata(exprStr)
         const result = expression.evaluate(inputJson)
         expect(result).toMatchSnapshot()
-        log('Output:', JSON.stringify(result,null, 2))
+        log(colors.green('Output:'), colors.green.bold(JSON.stringify(result,null, 2)))
     }
 }
