77f2857f2ca603be07cae1990d54c872bcc88309
Rodrigo Nascimento de Carvalho <rnascimentodecarva@unomaha.edu>
2019-04-02 11:31:06 -0300

    Updating color for Expr:

--- __tests__/jsonata-helper.js
+++ __tests__/jsonata-helper.js
@@ -6,7 +6,7 @@ const log = (...args) => (process.env.DEBUG ? console.log(...args) : () => {})
 module.exports = {
     testJsonataExpr: (inputJson, exprStr) => {
         log(colors.yellow('Input:'), colors.yellow.italic(JSON.stringify(inputJson, null, 2)))
-        log(colors.cyan('Expr:'), colors.cyan.underline(exprStr))
+        log(colors.bgWhite.black('Expr: ') + colors.bgWhite.black.underline(exprStr))
         const expression = jsonata(exprStr)
         const result = expression.evaluate(inputJson)
         expect(result).toMatchSnapshot()
